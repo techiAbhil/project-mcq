4243d673e8af85ae2ad96701caef43a18a40daf6
techiabhil <techiabhil@gmail.com>
2021-08-29 09:21:49 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<header>
+				<h3>Project MCQ</h3>
+			</header>
+
+			<main>
+				<section className="question">
+					<b>Question 1:</b>
+					<p>Is this supposed to be a question?</p>
+				</section>
+
+				<section className="options">
+					<div className="option">Answer 1: </div>
+					<div className="option">Answer 1: </div>
+					<div className="option">Answer 1: </div>
+					<div className="option">Answer 1: </div>
+				</section>
+
+				<section className="actions-center">
+					<button>Next</button>
+					<button>Previous</button>
+				</section>
+			</main>
+		</div>
+	);
 }
 
 export default App;
